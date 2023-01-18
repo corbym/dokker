e3c830b6fd8885c0e215454f3b57f6506a4a3131
mattcorby-eaglen <matt.corby-eaglen@sedex.com>
2023-01-18 12:24:04 +0000

    change pw

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ if (System.getenv()["OSSRH_PASSWORD"] != null) {
     nexusPublishing {
         repositories {
             sonatype {
-                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
                 snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
                 username.set("corbymatt") // defaults to project.properties["myNexusUsername"]
                 password.set(sonatypePassword) // defaults to project.properties["myNexusPassword"]
