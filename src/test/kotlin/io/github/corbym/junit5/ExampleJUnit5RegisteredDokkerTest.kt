42ea69088dd477b76f8110bf64ffd2e8307978cc
mattcorby-eaglen <matt.corby-eaglen@sedex.com>
2023-05-03 16:24:33 +0100

    small refactor

--- src/test/kotlin/io/github/corbym/junit5/ExampleJUnit5RegisteredDokkerTest.kt
+++ src/test/kotlin/io/github/corbym/junit5/ExampleJUnit5RegisteredDokkerTest.kt
@@ -4,24 +4,26 @@ import io.github.corbym.dokker.dokker
 import io.github.corbym.dokker.junit5.DokkerExtension
 import io.github.corbym.dokker.junit5.dokkerExtension
 import io.github.corbym.dokker.junit5.findFreePort
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
 import kotlin.test.assertTrue
 
 class ExampleJUnit5RegisteredDokkerTest {
     companion object {
+        val couchbasePort = findFreePort()
+
         @JvmStatic
         @RegisterExtension
         val server: DokkerExtension = dokkerExtension {
             container {
-                val freePort = findFreePort()
-
                 dokker {
                     name("couchbase")
                     detach()
                     debug()
-                    expose(freePort)
-                    publish(freePort to freePort)
+                    expose(couchbasePort)
+                    publish(couchbasePort to couchbasePort)
                     image { "arungupta/couchbase" }
                     version { "latest" }
                 }
@@ -32,5 +34,6 @@ class ExampleJUnit5RegisteredDokkerTest {
     @Test
     fun `can start couchbase docker container`() {
         assertTrue { server.hasStarted() }
+        assertThat(server.expose.first(), equalTo(couchbasePort))
     }
 }
