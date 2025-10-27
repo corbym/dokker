0f8b5faf72d09a00b787927a78ac0bf6963ff485
Matt <matt.corby@gmail.com>
2025-10-27 10:05:43 +0000

    Update Sonatype repository URLs in build.gradle.kts
    updating repo urls based on https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#getting-started-for-nexus-repository-manager-2-api-plugins
    
    (first try, lets see :) )

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,8 +92,8 @@ publishing {
     repositories {
         maven {
             name = "OSSRH"
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/service/local/snapshot/deploy/maven2/")
+            val releasesRepoUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
+            val snapshotsRepoUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
